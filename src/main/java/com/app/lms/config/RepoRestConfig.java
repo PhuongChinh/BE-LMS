59620f2a660b49aafc34c262a11651924ce0381e
phuongchinh <chinhbp010297@gmail.com>
2020-11-04 16:46:59 +0700

    init project

---
+++ src/main/java/com/app/lms/config/RepoRestConfig.java
@@ -0,0 +1,21 @@
+package com.app.lms.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.rest.core.config.RepositoryRestConfiguration;
+import org.springframework.data.rest.webmvc.config.RepositoryRestConfigurer;
+
+
+@Configuration
+public class RepoRestConfig implements RepositoryRestConfigurer {
+
+	@Override
+	public void configureRepositoryRestConfiguration(RepositoryRestConfiguration config) {
+		RepositoryRestConfigurer.super.configureRepositoryRestConfiguration(config);	
+		config.setReturnBodyForPutAndPost(true);
+		config.setReturnBodyOnCreate(true);
+		config.setReturnBodyOnUpdate(true);
+		config.setBasePath("/api/v1");
+	}
+	
+	
+}
